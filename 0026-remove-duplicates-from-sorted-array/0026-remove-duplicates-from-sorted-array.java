07388c5f943ac09cf2c33eb24636ccc6c4997bae
Akshay Jain <Jainakshay0804@gmail.com>
2026-08-13 00:24:25 +0530

    Time: 1 ms (77.3%), Space: 46.8 MB (24.8%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+         if (nums.length==0){
+            return 0;
+         }
+         int write=1;
+         for(int read =1; read<nums.length;read++)
+         {
+            if(nums[read] != nums[write-1])
+            {
+                nums[write]=nums[read];
+                write++;
+            }
+         }
+         return write;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
