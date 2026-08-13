ab3e23b895c0b316f5821742c3ab853ba97b2ae8
Akshay Jain <Jainakshay0804@gmail.com>
2026-08-14 00:58:36 +0530

    Time: 1 ms (97.71%), Space: 47 MB (90.22%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        int n=nums.length;
+        int [] result=new int [n*2];
+
+        for (int i=0;i<n;i++)
+        {
+           result[i] = nums[i];
+           result[i + n] = nums[i];
+        }
+
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
