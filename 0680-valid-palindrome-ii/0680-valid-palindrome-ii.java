4bcecbb811fb35d0f90587fc480f5d438126bfc6
Akshay Jain <Jainakshay0804@gmail.com>
2026-08-14 23:32:03 +0530

    Time: 4 ms (99.07%), Space: 47.9 MB (28.82%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public boolean validPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+
+                // Try deleting left OR deleting right
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    // This method is outside validPalindrome()
+    private boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
