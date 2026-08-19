5ee5783d453518f419a03135dfe971e43e56551e
Akshay Jain <Jainakshay0804@gmail.com>
2026-08-20 00:17:53 +0530

    Time: 2 ms (99.18%), Space: 119.6 MB (10.32%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int start=0;
+        int current=0;
+        int total=0;
+
+        for(int i=0;i<gas.length;i++)
+        {
+            int diff = gas[i]-cost[i];
+            total += diff;
+            current += diff;
+
+            if(current < 0)
+            {
+                start = i+1;
+                current =0;
+            }
+        }
+        if(total<0){
+            return -1;
+        }
+        return start;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
